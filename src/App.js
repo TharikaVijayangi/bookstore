cac6a112b4b6d3b84dc1c9a098ce2a07a5233e6a
TharikaVijayangi <it18016208@my.sliit.lk>
2021-02-22 09:46:38 +0530

    React Bootstrap Installation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
